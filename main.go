8aaf3dc49e1aac2e028bba6eea36971e119c9a8f
Alexey Baranov <baranov.alxy@gmail.com>
2015-07-17 12:05:40 +0600

    strong beat implemented

--- main.go
+++ main.go
@@ -18,21 +18,24 @@ const usage = `Rhygo,
 simple metronome with good clicking sound and timeout feature.
 
 Usage:
-    rhygo [-t <tempo>] [-o <timeout>]
+    rhygo [-t <tempo>] [-s <strong_beat>] [-o <timeout>]
 
 Options:
-    -t <tempo>   Metronome tempo.
-                 [default: 120]
-    -o <timeout> Program stop timeout in seconds.
-                 [default: 0]
+    -t <tempo>       Metronome tempo.
+                     [default: 120]
+    -s <strong_beat> Strong beat index.
+                     [default: 0]
+    -o <timeout>     Program stop timeout in seconds.
+                     [default: 0]
 `
 
 func main() {
 	args, _ := docopt.Parse(usage, nil, true, "1.0", false)
 
 	var (
-		tempo   = args["-t"].(string)
-		timeout = args["-o"].(string)
+		tempo           = args["-t"].(string)
+		timeout         = args["-o"].(string)
+		strongBeatIndex = args["-s"].(string)
 	)
 
 	timeoutInt, _ := strconv.Atoi(timeout)
@@ -40,17 +43,34 @@ func main() {
 		go exitOnTimeout(timeoutInt)
 	}
 
+	strongBeatIndexInt, _ := strconv.Atoi(strongBeatIndex)
+
 	tempoFloat, _ := strconv.ParseFloat(tempo, 64)
-	runMetronome(tempoFloat)
+
+	runMetronome(tempoFloat, strongBeatIndexInt)
 }
 
-func runMetronome(tempo float64) {
+func runMetronome(tempo float64, strongBeatIndex int) {
 	pulseAudio, pulseAudioContext := initPulseAudio()
 	defer pulseAudio.Dispose()
 	defer pulseAudioContext.Dispose()
 
+	tick := 0
+
 	for {
-		go performTickSound(pulseAudioContext)
+		sample := tickWeakSample
+
+		if strongBeatIndex != 0 {
+			if tick%strongBeatIndex == 0 {
+				sample = tickStrongSample
+				tick = 0
+			}
+		}
+
+		go performTickSound(sample, pulseAudioContext)
+		if strongBeatIndex != 0 {
+			tick++
+		}
 
 		sleepDuration, _ := time.ParseDuration(fmt.Sprintf(
 			"%fs", 60/tempo,
@@ -64,7 +84,7 @@ func exitOnTimeout(timeout int) {
 	os.Exit(0)
 }
 
-func performTickSound(context *pulsego.PulseContext) {
+func performTickSound(sample []int8, context *pulsego.PulseContext) {
 	stream := context.NewStream(
 		"default",
 		&pulsego.PulseSampleSpec{
@@ -82,7 +102,7 @@ func performTickSound(context *pulsego.PulseContext) {
 
 	count := 0
 	for {
-		stream.Write(tickWeakSample, pulsego.SEEK_RELATIVE)
+		stream.Write(sample, pulsego.SEEK_RELATIVE)
 
 		count++
 		if count == 200 {
